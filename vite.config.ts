9b6265e9818738803114f17295b19be0e752023c
linfang tong <l_tong@rococo.co.jp>
2026-03-14 13:54:15 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
-  // GitHub Pages のリポジトリ名が 'membercard' の場合はこれ
-  // もしパスが正しくない場合は、コンソールに 404 エラーが出ます
+  // GitHub Pages のリポジトリ名
   base: '/membercard/',
   
-  // ビルド時に型チェックをスキップしない設定
   build: {
     outDir: 'dist',
     emptyOutDir: true,
